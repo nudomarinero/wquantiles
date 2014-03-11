0ac597249cdf43a3c6ba1b489229e56ec830d150
José Sabater Montes <jsm@iaa.es>
2014-03-11 23:40:06 +0000

    Sanity checks in quantile_1D

--- weighted.py
+++ weighted.py
@@ -26,12 +26,28 @@ def quantile_1D(data, weights, quantile):
     quantile_1D : float
         The output value.
     """
+    # Check the data
+    if not isinstance(data, np.matrix) :
+        data = np.asarray(data)
+    if not isinstance(weights, np.matrix) :
+        weights = np.asarray(weights)
+    nd = data.ndim
+    if nd != 1:
+        raise TypeError("data must be a one dimensional array")
+    ndw = weights.ndim
+    if ndw != 1:
+        raise TypeError("weights must be a one dimensional array")
+    if data.shape != weights.shape:
+        raise TypeError("the length of data and weights must be the same")
+    if ((quantile > 1.) or (quantile < 0.)):
+        raise ValueError("quantile must have a value between 0. and 1.")
     # Sort the data
     ind_sorted = np.argsort(data)
     sorted_data = data[ind_sorted]
     sorted_weights = weights[ind_sorted]
     # Compute the auxiliary arrays
     Sn = np.cumsum(sorted_weights)
+    # TODO: Check that the weights do not sum zero
     Pn = (Sn-0.5*sorted_weights)/np.sum(sorted_weights)
     # Get the value of the weighted median
     return np.interp(quantile, Pn, sorted_data)
@@ -56,6 +72,7 @@ def quantile(data, weights, quantile):
         The output value.
     """
     # TODO: Extend to other shapes
+    # TODO: Allow to specify the axis 
     nx, ny, n = data.shape
     imr = data.reshape((nx*ny, n))
     result = np.apply_along_axis(quantile_1D, -1, imr, weights, quantile)
